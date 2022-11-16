5bb0724ecba05a13bda99cadd340d661ec1f1046
Klippie <info@johanvanderklift.nl>
2022-11-16 17:37:19 +0100

    Finished assignment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [ state, toggleState ] = React.useState(false);
+
   return (
-    <main className="off">
+    <main className={state ? "on" : "off"}>
       <section>
         <div className="dot"></div>
-        <button type="button">Turn on/off</button>
+        <button type="button" onClick={() => {
+          toggleState(!state);
+          console.log(state);
+        }}>{state ? "Turn off" : "Turn on"}</button>
       </section>
     </main>
   );
